00c47c6e858a80b9345e1a3e6d0fb1b42520e4cc
Gian Marco Romani <giaro710@gmail.com>
2020-11-16 17:41:14 +0100

    Fixed typo

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-import db from '../firebase';
+import db from "../firebase";
 
 import "../css/Chat.css";
 import StarBorderIcon from "@material-ui/icons/StarBorder";
@@ -9,17 +9,17 @@ import InfoIcon from "@material-ui/icons/Info";
 
 const Chat = () => {
   const { roomId } = useParams();
-  const [ roomDetails, setRoomDetails ] = useState(null);
+  const [roomDetails, setRoomDetails] = useState({});
 
   useEffect(() => {
     if (roomId) {
-      db.collection('rooms').document.(roomId).onSnapshot(snapshot => (
-        setRoomDetails(snapshot.data())
-      ))
+      db.collection("rooms")
+        .document(roomId)
+        .onSnapshot((snapshot) => setRoomDetails(snapshot.data()));
     }
   }, [roomId]);
 
-  console.log(roomDetails)
+  console.log(roomDetails);
 
   return (
     <div className="chat">
