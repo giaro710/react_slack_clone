18bcf37d498ce7a883567bd527741579c7332f97
Gian Marco Romani <giaro710@gmail.com>
2020-11-17 10:52:05 +0100

    Most of the style of the message done

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,12 +4,17 @@ import "../css/Message.css";
 const Message = ({ id, message, timestamp, user, userImage }) => {
   return (
     <div className="message" key={id}>
-      <img src={userImage} alt="User" />
+      <div className="message__image-container">
+        <img className="message__image" src={userImage} alt="User" />
+      </div>
       <div className="message__info">
         <h4>
-          {user} <span>timestamp</span>
+          {user}
+          <span className="message__timestamp">
+            {new Date(timestamp.toDate()).toUTCString()}
+          </span>
         </h4>
-        <p>{message}</p>
+        <p className="message__text">{message}</p>
       </div>
     </div>
   );
